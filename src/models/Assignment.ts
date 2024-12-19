df12164bd712e0a9d7e41ab61946585c37ab2809
Ishmael <darmfiel@andrew.cmu.edu>
2024-12-18 19:54:48 -0500

    created models for various collections

---
+++ src/models/Assignment.ts
@@ -0,0 +1,67 @@
+import mongoose, { Schema, Types } from 'mongoose';
+
+export type IRubricItem = {
+  _id?: mongoose.Types.ObjectId;
+  description: string;
+  points: number;
+}
+
+export type IProblem = {
+  _id?: Types.ObjectId;  // Optional for new problems, present for existing ones
+  question: string;
+  maxPoints: number;
+  orderIndex: number;
+  rubric: {
+    items: IRubricItem[];
+  }
+}
+
+export type IAssignment = {
+  _id?: mongoose.Types.ObjectId;
+  courseId: mongoose.Types.ObjectId;
+  title: string;
+  description: string;
+  dueDate: Date;
+  lateDueDate: Date;
+  status: 'unreleased' | 'released' | 'closed' | 'graded';
+  totalPoints: number;
+  problems: IProblem[];
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const rubricItemSchema = new Schema<IRubricItem>({
+  description: { type: String, required: true },
+  points: { type: Number, required: true }
+});
+
+const problemSchema = new Schema<IProblem>({
+  question: { type: String, required: true },
+  maxPoints: { type: Number, required: true },
+  orderIndex: { type: Number, required: true },
+  rubric: {
+    items: [rubricItemSchema]
+  }
+});
+
+const assignmentSchema = new Schema<IAssignment>({
+  courseId: { type: Schema.Types.ObjectId, ref: 'Course' },
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  dueDate: { type: Date, required: true },
+  lateDueDate: { type: Date, required: true },
+  status: { 
+    type: String, 
+    enum: ['unreleased', 'released', 'closed', 'graded'],
+    default: 'unreleased',
+    required: true
+  },
+  totalPoints: { type: Number, required: true },
+  problems: [problemSchema]
+}, {
+  timestamps: true
+});
+
+const Assignment = mongoose.models.Assignment || mongoose.model<IAssignment>('Assignment', assignmentSchema);
+
+export { Assignment };
---
+++ src/models/Submission.ts
@@ -0,0 +1,37 @@
+import mongoose, { Schema } from 'mongoose';
+import { IRubricItem } from '@/models/Assignment';
+
+export type ISubmission = {
+  _id?: mongoose.Types.ObjectId;
+  assignmentId: mongoose.Types.ObjectId;
+  problemId: mongoose.Types.ObjectId;
+  studentId: mongoose.Types.ObjectId;
+  answer: string;
+  submittedAt: Date;
+  graded: boolean;
+  earnedPoints?: number;
+  gradedBy?: mongoose.Types.ObjectId; // Reference to User
+  gradedAt?: Date;
+  appliedRubricItems?: IRubricItem[]; // Reference to the RubricItems
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const submissionSchema = new Schema<ISubmission>({
+  assignmentId: { type: Schema.Types.ObjectId, ref: 'Assignment', required: true },
+  problemId: { type: Schema.Types.ObjectId, ref: 'Assignment.problems', required: true },
+  studentId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  answer: { type: String, required: true },
+  submittedAt: { type: Date, required: true },
+  graded: { type: Boolean, required: true },
+  earnedPoints: { type: Number },
+  gradedBy: { type: Schema.Types.ObjectId, ref: 'User' },
+  gradedAt: { type: Date },
+  appliedRubricItems: [{ type: Schema.Types.ObjectId, ref: 'Assignment.problems.rubric.items' }]
+}, {
+  timestamps: true
+});
+
+const Submission = mongoose.models.Submission || mongoose.model<ISubmission>('Submission', submissionSchema);
+
+export { Submission };
