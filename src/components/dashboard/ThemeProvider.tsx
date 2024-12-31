1cfa4a16259637d43291b652cf0807234eb3b6ce
Ishmael <darmfiel@andrew.cmu.edu>
2024-12-31 03:32:04 -0500

    made default theme light mode

--- src/components/dashboard/ThemeProvider.tsx
+++ src/components/dashboard/ThemeProvider.tsx
@@ -5,6 +5,9 @@ import { ThemeProvider as NextThemesProvider } from "next-themes"
 import { type ThemeProviderProps } from "next-themes"
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  return (
+    <NextThemesProvider themes={['light', 'dark', 'custom']} defaultTheme="light" {...props}>
+      {children} 
+    </NextThemesProvider>
+  );
 }
-
